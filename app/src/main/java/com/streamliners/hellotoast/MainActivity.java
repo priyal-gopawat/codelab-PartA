fb27d537a562fd1533e6d74af0c74a5116a97385
priyal-gopawat <priyalgopawat11111@gmail.com>
2021-05-13 20:57:52 +0530

    comments

--- app/src/main/java/com/streamliners/hellotoast/MainActivity.java
+++ app/src/main/java/com/streamliners/hellotoast/MainActivity.java
@@ -18,12 +18,20 @@ public class MainActivity extends AppCompatActivity {
         mShowCount = (TextView) findViewById(R.id.show_count);
    }
 
+    /**
+     * Show toast on the  bottom of the screen for a short period of time
+     * @param view
+     */
     public void showToast(View view) {
         Toast toast = Toast.makeText(this,R.string.toast_message,Toast.LENGTH_SHORT);
         toast.show();
 
     }
 
+    /**
+     * increase the counter by 1
+     * @param view
+     */
     public void countUp(View view) {
         ++mCount;
         if (mShowCount != null)
